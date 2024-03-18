ed6715c797ea959f13230941091ba6698c9dbc36
Un Kyu Lee <unkyulee@gmail.com>
2024-03-18 17:08:05 +0100

    keymap

--- micro-journal-rev-4-esp32/src/service/keyboard/keyboard.cpp
+++ micro-journal-rev-4-esp32/src/service/keyboard/keyboard.cpp
@@ -8,15 +8,36 @@
 #define COLS 10  // columns
 
 // define the symbols on the buttons of the keypads
+/*
 char keys[ROWS][COLS] = {
     {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
     {10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
     {20, 21, 22, 23, 24, 25, 26, 27, 28, 29}};
+*/
+// prettier-ignore
+char keys[COLS][ROWS] = {
+    {20, 10, 0},
+    {21, 11, 1},
+    {22, 12, 2},
+    {23, 13, 3},
+    {24, 14, 4},
+    {25, 15, 5},
+    {26, 16, 6},
+    {27, 17, 7},
+    {28, 18, 8},
+    {29, 19, 9}};
 
-byte rowPins[ROWS] = {16, 17, 5};                              // connect to the row pinouts of the keypad
-byte colPins[COLS] = {13, 12, 14, 27, 26, 25, 33, 32, 21, 22}; // connect to the column pinouts of the keypad
+// connect to the row pinouts of the keypad
+byte rowPins[ROWS] = {23, 22, 21};
+// connect to the column pinouts of the keypad
+byte colPins[COLS] = {19, 18, 5, 17, 16, 12, 14, 27, 26, 25};
 
-Adafruit_Keypad customKeypad = Adafruit_Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Adafruit_Keypad customKeypad = Adafruit_Keypad(makeKeymap(keys), colPins, rowPins, COLS, ROWS);
+
+#define FN 28
+#define SHIFT 29
+#define EMPTY 0
+#define MENU 1
 
 // layers
 // prettier-ignore
@@ -24,24 +45,23 @@ int layers[LAYERS][ROWS * COLS] = {
     {// normal layers
      'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
      'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\n',
-     'z', 'x', 'c', 'v', ' ', 'b', 'n', 'm', -1, 1},
+     'z', 'x', 'c', 'v', ' ', 'b', 'n', 'm', FN, SHIFT},
     {// when shift is pressed
      'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
      'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '\n',
-     'Z', 'X', 'C', 'V', ' ', 'B', 'N', 'M', -1, 1},
+     'Z', 'X', 'C', 'V', ' ', 'B', 'N', 'M', FN, SHIFT},
     {// when number layer key is pressed
      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-     '`', '\\', 0, '-', '=', '[', ']', ';', '\'', '\n',
-     0, 0, 0, 0, ' ', ',', '.', '/', -1, 1},
+     '`', '\\', '-', '=', '[', ']', ';', '\'', EMPTY, '\n',
+     MENU, EMPTY, EMPTY, EMPTY, ' ', ',', '.', '/', FN, SHIFT},
     {// when number layer key is pressed
      '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
-     '~', '|', 0, '_', '+', '{', '}', ':', '\"', '\n',
-     0, 0, 0, 0, ' ', '<', '>', '?', -1, 1}};
+     '~', '|', '_', '+', '{', '}', ':', '\"', EMPTY, '\n',
+     MENU, EMPTY, EMPTY, EMPTY, ' ', '<', '>', '?', FN, SHIFT}};
 
 void keyboard_setup_main()
 {
     customKeypad.begin();
-    network_enable(true);
 }
 void keyboard_setup_secondary() {}
 
@@ -61,7 +81,10 @@ void keyboard_loop_main()
         {
             keypadEvent e = customKeypad.read();
             int key = keyboard_get_key(e);
-            if (key > 1)
+            if(key == MENU) {
+                Serial.println("MENU KEY PRESSED");
+            }
+            else if (key != EMPTY)
             {
                 Serial.print((char)key);
                 if (e.bit.EVENT == KEY_JUST_PRESSED)
@@ -74,26 +97,60 @@ void keyboard_loop_main()
 }
 void keyboard_loop_secondary() {}
 
+bool _shift_pressed = false;
+bool _fn_pressed = false;
 int keyboard_get_key(keypadEvent e)
 {
-    // only handle event when key is pressed
-    if (e.bit.EVENT != KEY_JUST_PRESSED)
-        return 0;
-
-    // define the layer
-    int layer = 0;
-
-    // check if the layer key is pressed
-    if (e.bit.KEY == -1)
+    //
+    // step 1. layer processing
+    //
+    if (e.bit.KEY == FN)
     {
-        layer = 2;
+        if (e.bit.EVENT == KEY_JUST_PRESSED)
+        {
+            _fn_pressed = true;
+        }
+        else
+        {
+            _fn_pressed = false;
+        }
+    }
+    else if (e.bit.KEY == SHIFT)
+    {
+        if (e.bit.EVENT == KEY_JUST_PRESSED)
+        {
+            _shift_pressed = true;
+        }
+
+        else if (e.bit.EVENT == KEY_JUST_RELEASED)
+        {
+            _shift_pressed = false;
+        }
     }
 
-    // if SHIFT is pressed increase the layer by one
-    if (e.bit.KEY == 1)
+    // stop here
+    if (e.bit.KEY == SHIFT || e.bit.KEY == FN)
     {
-        layer += 1;
+        return EMPTY;
     }
 
-    return layers[layer][e.bit.KEY];
+    // step 2. process the key
+    if (e.bit.EVENT == KEY_JUST_PRESSED)
+    {
+        // define the layer
+        int layer = 0;
+
+        // check if the layer key is pressed
+        if (_fn_pressed)
+            layer = 2;
+        // check if the shift key is pressed
+        if (_shift_pressed)
+            layer += 1;
+
+        // return the corresponding key
+        return layers[layer][e.bit.KEY];
+    }
+
+    //Serial.printf("KEY: %d EVENT: %d\n", e.bit.KEY, e.bit.EVENT);
+    return EMPTY;
 }
--- micro-journal-rev-4-esp32/src/service/service.cpp
+++ micro-journal-rev-4-esp32/src/service/service.cpp
@@ -4,26 +4,26 @@
 
 void service_setup_main()
 {
-    display_setup_main();
+    //display_setup_main();
     keyboard_setup_main();
 }
 
 void service_setup_secondary() 
 {
-    display_setup_secondary();
+    //display_setup_secondary();
     keyboard_setup_secondary();
 }
 
 // place the service loops that is critical and should be performed in timely manner
 void service_loop_main()
 {
-    display_loop_main();
+    //display_loop_main();
     keyboard_loop_main();
 }
 
 // place the service loops that doesn't affect the main functionality
 void service_loop_secondary()
 {
-    display_loop_secondary();
+    //display_loop_secondary();
     keyboard_loop_secondary();
 }
