05e836c6c65e54c70c180dcc29202a351172ad23
Un Kyu Lee <unkyulee@gmail.com>
2025-06-30 22:09:14 +0200

    handling knob ec11

--- micro-journal-rev-4-esp32/src/keyboard/InputMethod/Knob/Knob.cpp
+++ micro-journal-rev-4-esp32/src/keyboard/InputMethod/Knob/Knob.cpp
@@ -1,44 +1,75 @@
 #include "Knob.h"
+#include "display/display.h"
 
-#include <a21.hpp>
-using namespace a21;
+#include <RotaryEncoder.h>
 
-EC11 encoder;
-const int encoderPinA = 22;
-const int encoderPinB = 26;
+RotaryEncoder *encoder = nullptr;
 
-void pinDidChange()
+// This interrupt routine will be called on any change of one of the input signals
+void checkPosition()
 {
-    encoder.checkPins(digitalRead(encoderPinA), digitalRead(encoderPinB));
+    encoder->tick(); // just call tick() to check the state.
 }
 
 void knob_setup()
 {
-    attachInterrupt(digitalPinToInterrupt(encoderPinA), pinDidChange, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(encoderPinB), pinDidChange, CHANGE);
+    encoder = new RotaryEncoder(22, 26, RotaryEncoder::LatchMode::TWO03);
+    // register interrupt routine
+    attachInterrupt(digitalPinToInterrupt(22), checkPosition, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(26), checkPosition, CHANGE);
 }
-
-static int value = 0;
 
 void knob_loop()
 {
-    EC11Event e;
-    if (encoder.read(&e))
+    static int lastPos = 0;
+    static int lastDir = 0;
+    static int dirCount = 0;
+    static unsigned long lastMoveTime = 0;
+
+    encoder->tick(); // just call tick() to check the state.
+
+    int newPos = encoder->getPosition();
+    int newDir = (int)encoder->getDirection();
+
+    if (newPos != lastPos)
     {
+        unsigned long now = millis();
 
-        // OK, got an event waiting to be handled, its count field is number of steps since the last check.
-
-        if (e.type == EC11Event::StepCW)
+        if (newDir == lastDir && newDir != 0)
         {
-            // Clock-wise.
-            value += e.count;
+            dirCount++;
+            if (dirCount == 2)
+            {
+                Serial.print("Valid rotation detected! Direction: ");
+                Serial.println(newDir);
+                dirCount = 0; // Reset after valid rotation
+
+                // newDir - 1 UP
+                // newDir - -1 DOWN
+                if (newDir == 1)
+                {
+                    display_keyboard(20, true);
+                }
+                else if (newDir == -1)
+                {
+                    display_keyboard(21, true);
+                }
+            }
         }
         else
         {
-            // Counter clock-wise.
-            value -= e.count;
+            dirCount = 1; // Start new count for new direction
         }
 
-        Serial.println(value);
+        lastDir = newDir;
+        lastMoveTime = now;
+        lastPos = newPos;
+    }
+
+    // Reset if no action within 1 second
+    if (dirCount > 0 && (millis() - lastMoveTime > 1000))
+    {
+        dirCount = 0;
+        lastDir = 0;
     }
 }
